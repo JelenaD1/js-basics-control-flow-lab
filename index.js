b0c9bc59a95117d8e20d13e9bb5f488223102737
Jelena <jelena.durovic@powerhrg.com>
2021-05-20 20:43:03 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if (someNumber <=400){
+    return "This one is on me!"
+  } else if (someNumber > 2000 && someNumber <= 2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else if (someNumber > 2500){
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+
   // Write your code here!
+
+function ternaryCheckCity(city){
+  const checkCity = city === "NYC" ? "Ok, sounds good." : "No go."
+  return checkCity
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  let comment;
+  switch (tip) {
+    case "generous":
+      comment = "Thank you so much.";
+      break;
+    case "not as generous":
+      comment = "Thank you.";
+      break;
+    case "thanks for everything":
+      comment = "Bye.";
+      break;
+      }
+      return comment 
+    }
